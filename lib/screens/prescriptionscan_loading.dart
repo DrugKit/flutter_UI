4fd1049ae1dd7ea28780a06d66cb9fa4add1adba
doaa <doaaletwam919@gmail.com>
2025-04-25 02:25:21 +0300

    prescriptionLoadingScreen

--- lib/screens/prescriptionscan_loading.dart
+++ lib/screens/prescriptionscan_loading.dart
@@ -5,48 +5,64 @@ class PrescriptionLoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFFFFFFF),
       appBar: AppBar(
-        leading: BackButton(color: Colors.black),
+        leading: BackButton(color: const Color(0xFF0C1467)), 
         title: Text(
           "prescription scan",
-          style: TextStyle(color: Colors.indigo),
+          style: TextStyle(
+            color: const Color(0xFF0C1467),
+            fontWeight: FontWeight.bold,
+          ),
         ),
         centerTitle: true,
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.white, // AppBar أبيض
         elevation: 0,
       ),
-      backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: ElevatedButton.icon(
-              onPressed: () {
-                // Future.delayed(Duration(seconds: 2), () {
-                //   Navigator.push(
-                //     context,
-                //     MaterialPageRoute(
-                //       builder: (_) => PrescriptionResultScreen(),
-                //     ),
-                //   );
-                // });
-              },
-              icon: Icon(Icons.upload_file),
-              label: Text("Upload Your Prescription"),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.indigo,
-                minimumSize: Size(double.infinity, 50),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // الزر فوق
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 40, 20, 0),
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    // Future.delayed(Duration(seconds: 2), () {
+                    //   Navigator.push(
+                    //     context,
+                    //     MaterialPageRoute(
+                    //         builder: (_) => PrescriptionResultScreen()),
+                    //   );
+                    // });
+                  },
+                  icon: Icon(Icons.upload_file, color: Colors.white),
+                  label: Text(
+                    "Upload Your Prescription",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF0C1467),
+                    minimumSize: Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
                 ),
               ),
-            ),
+            ],
           ),
-          SizedBox(height: 50),
-          CircularProgressIndicator(
-            color: Colors.indigo,
-            strokeWidth: 6,
+
+          // loader
+          Center(
+            child: CircularProgressIndicator(
+              color: Colors.indigo,
+              strokeWidth: 5,
+            ),
           ),
         ],
       ),
